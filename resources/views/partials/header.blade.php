716737760937b764bec63feb7e573670b4bd3835
MaionePietro <pietromaionespam@gmail.com>
2023-04-11 02:36:14 +0200

    FEAT header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header class="container header_list">
+    <div class="logo">
+        <img src="{{ Vite::asset('resource/img/dc-logo.png') }}" alt="">
+    </div>
+    <div class="list">
+        <ul>
+            <li><a href="">CHARACTES</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIE</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAME</a></li>
+            <li><a href="">COLLECTIBLE</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+    </div>
+</header>
